ce8319fcb7b07bb2b0d5f5d315dee9416f7305dd
1G4S <igor.sarnowski.21@gmail.com>
2024-09-21 12:16:38 +0200

    test: adding test for get_stadiums function from extract.py

--- test/test_extract.py
+++ test/test_extract.py
@@ -13,8 +13,8 @@ class TestExtractFunctions(unittest.TestCase):
         Mocks the requests.get call to simulate an API response and verifies that the
         get_teams method returns the expected JSON data without making an actual API call.
 
-        Args:
-            mock_get (Mock): A mock object that simulates the requests.get method.
+        :param mock_get:
+            A mock object that simulates the requests.get method.
 
         """
         mock_response = {'teams': []}
@@ -24,5 +24,26 @@ class TestExtractFunctions(unittest.TestCase):
 
         extract = Extract()
         result = extract.get_teams()
+
+        self.assertEqual(result, mock_response)
+
+    @patch('requests.get')
+    def test_get_stadiums(self, mock_get):
+        """
+        Tests the get_stadiums method to ensure it correctly fetches team data from the API.
+
+        Mocks the requests.get call to simulate an API response and verifies that the
+        get_stadiums method returns the expected JSON data without making an actual API call.
+
+        :param mock_get:
+            A mock object that simulates the requests.get method.
+        """
+        mock_response = {'stadiums': []}
+
+        mock_get.return_value = Mock(status_code=200)
+        mock_get.return_value.json.return_value = mock_response
+
+        extract = Extract()
+        result = extract.get_stadiums()
 
         self.assertEqual(result, mock_response)
