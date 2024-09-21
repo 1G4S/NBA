10cdca9fda1022d6edefe7104a1c0b953506caf3
1G4S <igor.sarnowski.21@gmail.com>
2024-09-21 10:51:53 +0200

    feat: Adding get_teams function to get info about teams from API

--- extract.py
+++ extract.py
@@ -1,26 +1,34 @@
+import requests
+
+
 class Extract:
 
-    '''
-    klucz API -> .env -> dotenv library -> enviromental variables
-    endpoints -> strings
-    request method -> (pobiera endpoint -> waliduje output -> zwraca json object) -> try/except -> pusty JSON/throw error
-
-    1 funkcja = 1 funkcjonalnosc -> unittest
-    '''
     def __init__(self):
-        self.API_KEY = None
+        self.API_KEY = "508f3dee654d4f5b8b06b9fe48bbb51e"
         self.ENDPOINT_MAIN = "https://api.sportsdata.io/v3/"
-        self.endpoint_matches = None
-        self.endpoint_stadiums = None
-        self.endpoint_teams = None
+        self.endpoint_players = "nba/scores/json/Players"
+        self.endpoint_stadiums = "nba/scores/json/Stadiums"
+        self.endpoint_teams = "nba/scores/json/teams"
+        self.teams = ["/WAS", "/CHA", "/ATL", "/MIA", "/ORL", "/NY", "/PHI", "/BKN", "/BOS",
+                      "/TOR", "/CHI", "/CLE", "/IND", "/DET", "/MIL", "/MIN", "/UTA", "/OKC",
+                      "/POR", "/DEN", "/MEM", "/HOU", "/NO", "/SA", "/DAL", "/GS", "/LAL", "/LAC",
+                      "/PHO", "/SAC"]
 
+    def get_teams(self):
+        """
+        Retrieves team data from the teams endpoint.
 
-    def example_func(self):
-        '''
-        example description
+        This function constructs a URL using the base endpoint and the teams-specific
+        endpoint, then sends a GET request with the API key as a parameter. It returns
+        the data in JSON format.
 
-        :param:
-        :return:
-        '''
-        pass
+        Params:
+            None
 
+        Returns:
+            dict: The JSON response containing team data.
+        """
+        url = self.ENDPOINT_MAIN + self.endpoint_teams
+        params = {'key': self.API_KEY}
+        response = requests.get(url=url, params=params)
+        return response.json()
