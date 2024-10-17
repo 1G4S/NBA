2ca5a103fdc20be0dbaad01cdb0d4d855afaae4c
1G4S <igor.sarnowski.21@gmail.com>
2024-10-17 10:11:51 +0200

    feat: apllying strategy to players

--- extract/extract.py
+++ extract/extract.py
@@ -62,53 +62,53 @@ class Extract:
         except RequestException as e:
             print(f"Error fetching stadium data: {e}")
             return None
-
-    def get_players(self):
-        """
-        Retrieves players data from players endpoint.
-
-        This function constructs the URL by combining the base endpoint, the players-specific
-        endpoint, and the team-specific shortcut for each team. It uses a for loop to send a GET request
-        for each team with the API key as a parameter and collects the players' data.
-        If a request is successful, the JSON response is parsed and added to a list.
-        In case of a request or JSON parsing error,
-        the team entry will have a value of None.
-
-        :param:
-            None
-
-        :return:
-            list: The JSON response containing players data for each team.
-        """
-        all_players = []
-        for team in self.get_list_of_teams():
-            url = self.ENDPOINT_MAIN + self.endpoint_players + "/" + team
-            params = {'key': self.API_KEY}
-            try:
-                response = self.session.get(url=url, params=params)
-                response.raise_for_status()
-                all_players.extend(response.json())
-
-            except RequestException as e:
-                print(f"Error fetching fata for {team}: {e}")
-                all_players = None
-
-        return all_players
-
-    def get_list_of_teams(self):
-        """
-        Get list of teams from json file.
-
-        This function read data from json file and returns list of teams
-        from NBA, if loading data is successful, if not, None will be returned.
-        :return:
-            list: List of teams from NBA.
-        """
-        try:
-            with open(self.filename, 'r') as file:
-                data = json.load(file)
-                return data['Teams']
-        except Exception as e:
-            print(e)
-            return None
-
+    #
+    # def get_players(self):
+    #     """
+    #     Retrieves players data from players endpoint.
+    #
+    #     This function constructs the URL by combining the base endpoint, the players-specific
+    #     endpoint, and the team-specific shortcut for each team. It uses a for loop to send a GET request
+    #     for each team with the API key as a parameter and collects the players' data.
+    #     If a request is successful, the JSON response is parsed and added to a list.
+    #     In case of a request or JSON parsing error,
+    #     the team entry will have a value of None.
+    #
+    #     :param:
+    #         None
+    #
+    #     :return:
+    #         list: The JSON response containing players data for each team.
+    #     """
+    #     all_players = []
+    #     for team in self.get_list_of_teams():
+    #         url = self.ENDPOINT_MAIN + self.endpoint_players + "/" + team
+    #         params = {'key': self.API_KEY}
+    #         try:
+    #             response = self.session.get(url=url, params=params)
+    #             response.raise_for_status()
+    #             all_players.extend(response.json())
+    #
+    #         except RequestException as e:
+    #             print(f"Error fetching fata for {team}: {e}")
+    #             all_players = None
+    #
+    #     return all_players
+    #
+    # def get_list_of_teams(self):
+    #     """
+    #     Get list of teams from json file.
+    #
+    #     This function read data from json file and returns list of teams
+    #     from NBA, if loading data is successful, if not, None will be returned.
+    #     :return:
+    #         list: List of teams from NBA.
+    #     """
+    #     try:
+    #         with open(self.filename, 'r') as file:
+    #             data = json.load(file)
+    #             return data['Teams']
+    #     except Exception as e:
+    #         print(e)
+    #         return None
+    #
