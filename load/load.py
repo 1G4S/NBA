1d775612ff00f2f613d614d67f70956d5aa57a08
1G4S <igor.sarnowski.21@gmail.com>
2024-10-23 07:55:25 +0200

    feature: adding strategy to load

--- load/load.py
+++ load/load.py
@@ -1,29 +1,30 @@
-from sqlalchemy import create_engine
+from load.strategies.abstract_load_strategy import LoadStrategy
 
-from extract.extract import Extract
-from extract.strategies.players_retrieval_strategy import PlayersRetrievalStrategy
-from extract.strategies.stadiums_retrieval_strategy import StadiumsRetrievalStrategy
-from transform.strategies.players_strategy import PlayersStrategy
-from transform.strategies.stadiums_strategy import StadiumsStrategy
-from transform.transform import Transform
 
-connection_string = 'postgresql+psycopg2://igorsarnowski:S3o3c3c3e3r3@localhost:5432/NBA'
-engine = create_engine(connection_string)
+class Load:
+    def __init__(self, strategy: LoadStrategy):
+        """
+        :param strategy: Instance of LoadStrategy that defines the data loading strategy to be used.
+        """
+        self._strategy = strategy
 
-stadiums_retrieval_strategy = StadiumsRetrievalStrategy()
-stadiums_extract = Extract(stadiums_retrieval_strategy)
-stadiums_strategy = StadiumsStrategy()
-transform1 = Transform(stadiums_strategy, stadiums_extract)
+    @property
+    def strategy(self) -> LoadStrategy:
+        """
+        :return: The current load strategy being employed
+        """
+        return self._strategy
 
-players_retrieval_strategy = PlayersRetrievalStrategy()
-players_extract = Extract(players_retrieval_strategy)
-players_strategy = PlayersStrategy()
-transform2 = Transform(players_strategy, players_extract)
+    @strategy.setter
+    def strategy(self, strategy=LoadStrategy) -> None:
+        """
+        :param strategy: The loading strategy to be used, an instance of LoadStrategy.
+        :return: None
+        """
+        self._strategy = strategy
 
-df1 = transform1.get_clean_data()
-df2 = transform2.get_clean_data()
-df1.to_sql("stadiums", con=engine, if_exists='replace')
-df2.to_sql("players", con=engine, if_exists='replace')
-
-# general conn function -> create_connection_engine -> param: (postgres, mysql, mssql) -> conn_string -> engine
-# load -> param: engine -> exec to_sql -> end
+    def load_prepared_data(self):
+        """
+        :return: Load the data to database by the current strategy.
+        """
+        self._strategy.load()
